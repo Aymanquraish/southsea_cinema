993d49860cee31ae2ce351189554ed4b380a2c4c
Aymanquraish <aymanquraish89@gmail.com>
2026-09-24 20:13:17 +0100

    Add ticket quantity dropdown menu

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:southsea_cinema/constants.dart';
 import 'package:southsea_cinema/widgets/nav_drawer.dart';
 
-class MovieListing extends StatelessWidget {
+class MovieListing extends StatefulWidget {
   const MovieListing({super.key});
+
+  @override
+  State<MovieListing> createState() => _MovieListingState();
+}
+
+class _MovieListingState extends State<MovieListing> {
+  int selectedQuantity = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +38,22 @@ class MovieListing extends StatelessWidget {
                 Text('Runtime: 135 minutes'),
                 Text('Age Rating: 12A'),
               ],
+            ),
+            DropdownMenu<int>(
+              initialSelection: 1,
+              label: const Text('Tickets'),
+              dropdownMenuEntries: const [
+                DropdownMenuEntry(value: 1, label: '1'),
+                DropdownMenuEntry(value: 2, label: '2'),
+                DropdownMenuEntry(value: 3, label: '3'),
+                DropdownMenuEntry(value: 4, label: '4'),
+                DropdownMenuEntry(value: 5, label: '5'),
+              ],
+              onSelected: (value) {
+                setState(() {
+                  selectedQuantity = value ?? 1;
+                });
+              },
             ),
           ],
         ),
